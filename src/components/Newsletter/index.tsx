56ca68e3ca38f525e9795099f008f9d73f748a3c
mtzzwr <mateus.gsep@gmail.com>
2021-05-29 14:55:13 -0300

    creating input validations

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,7 +1,45 @@
+import { useState } from 'react'
 import { Input } from 'semantic-ui-react'
+import * as EmailValidator from 'email-validator';
 import './styles.css'
 
 const Newsletter = () => {
+
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [errorEmail, setErrorEmail] = useState<boolean>(true)
+    const [errorName, setErrorName] = useState<boolean>(true)
+
+    const validInputs = () => {
+        if (name.length < 3) {
+            setErrorName(false);
+        }
+
+        if (!EmailValidator.validate(email)) {
+            setErrorEmail(false)
+        }
+    }
+
+    const handleSubmit = () => {
+
+        if (name.length < 3) {
+            setErrorName(false);
+        } else {
+            setErrorName(true)
+        }
+
+        if (!EmailValidator.validate(email)) {
+            setErrorEmail(false)
+        } else {
+            setErrorEmail(true)
+        }
+
+        console.log("nome", name)
+        console.log("email", email)
+        console.log("erro nome", errorName)
+        console.log("erro email", errorEmail)
+
+    }
 
     return (
         <div id="newsletter_container">
@@ -11,9 +49,19 @@ const Newsletter = () => {
                 </h4>
             </div>
             <div id="newsletter_form">
-                <Input placeholder='Digite seu nome' />
-                <Input placeholder='Digite seu e-mail...' />
-                <button id="btn_send">Eu quero!</button>
+                <div className="input_container">
+                    <Input onChange={(name_digitted) => {
+                        setName(name_digitted.target.value)
+                    }} placeholder='Digite seu nome' />
+                    <span hidden={errorName} className="error_msg">Preencha com seu nome completo</span>
+                </div>
+                <div className="input_container">
+                    <Input onChange={(email_digitted) => {
+                        setEmail(email_digitted.target.value)
+                    }} placeholder='Digite seu e-mail...' />
+                    <span hidden={errorEmail} className="error_msg">Preencha com um e-mail válido</span>
+                </div>
+                <button id="btn_send" onClick={handleSubmit}>Eu quero!</button>
             </div>
         </div>
     )
