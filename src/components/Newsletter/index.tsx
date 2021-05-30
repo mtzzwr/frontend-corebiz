b4847aac692150cadbc2202ebf42b66c3ae7a2c2
mtzzwr <mateus.gsep@gmail.com>
2021-05-29 22:18:54 -0300

    finished newsletter form validation and sending to api

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,69 +1,89 @@
 import { useState } from 'react'
-import { Input } from 'semantic-ui-react'
 import * as EmailValidator from 'email-validator';
 import './styles.css'
+import axios from 'axios';
+import { BASE_URL } from '../../utils/requests';
 
 const Newsletter = () => {
 
     const [name, setName] = useState<string>('')
     const [email, setEmail] = useState<string>('')
-    const [errorEmail, setErrorEmail] = useState<boolean>(true)
-    const [errorName, setErrorName] = useState<boolean>(true)
+    const [errorEmail, setErrorEmail] = useState<string>('')
+    const [errorName, setErrorName] = useState<string>('')
+    const [registered, setRegistered] = useState<boolean>(false)
 
-    const validInputs = () => {
-        if (name.length < 3) {
-            setErrorName(false);
-        }
-
-        if (!EmailValidator.validate(email)) {
-            setErrorEmail(false)
+    const validateInputs = () => {
+        setErrorEmail('')
+        setErrorName('')
+        if (name.length < 3 && !EmailValidator.validate(email)) {
+            setErrorName('Preencha com seu nome completo')
+            setErrorEmail('Preencha com um email válido')
+            return false
+        } else if (!EmailValidator.validate(email)) {
+            setErrorEmail('Preencha com um email válido')
+            return false
+        } else if (name.length < 3) {
+            setErrorName('Preencha com seu nome completo')
+            return false
+        } else {
+            return true
         }
     }
 
     const handleSubmit = () => {
-
-        if (name.length < 3) {
-            setErrorName(false);
-        } else {
-            setErrorName(true)
+        if (validateInputs()) {
+            axios.post(`${BASE_URL}/newsletter`, {
+                email: email,
+                name: name
+            })
+                .then(response => {
+                    console.log(response)
+                    setRegistered(true)
+                    setName('')
+                    setEmail('')
+                })
         }
-
-        if (!EmailValidator.validate(email)) {
-            setErrorEmail(false)
-        } else {
-            setErrorEmail(true)
-        }
-
-        console.log("nome", name)
-        console.log("email", email)
-        console.log("erro nome", errorName)
-        console.log("erro email", errorEmail)
-
     }
 
     return (
-        <div id="newsletter_container">
-            <div>
-                <h4>
-                    Participe de nossas news com promoções e novidades!
-                </h4>
-            </div>
-            <div id="newsletter_form">
-                <div className="input_container">
-                    <Input onChange={(name_digitted) => {
-                        setName(name_digitted.target.value)
-                    }} placeholder='Digite seu nome' />
-                    <span hidden={errorName} className="error_msg">Preencha com seu nome completo</span>
-                </div>
-                <div className="input_container">
-                    <Input onChange={(email_digitted) => {
-                        setEmail(email_digitted.target.value)
-                    }} placeholder='Digite seu e-mail...' />
-                    <span hidden={errorEmail} className="error_msg">Preencha com um e-mail válido</span>
-                </div>
-                <button id="btn_send" onClick={handleSubmit}>Eu quero!</button>
-            </div>
-        </div>
+        <>
+            {registered ? (
+                <>
+                    <div id="registered">
+                        <h4>Seu e-mail foi cadastrado com sucesso!</h4>
+                        <span>A partir de agora você receberá as novidades e ofertas exclusivas</span>
+                        <button onClick={() => {
+                            setRegistered(false)
+                        }}>Cadastrar novo e-mail</button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div id="newsletter_container">
+                        <div>
+                            <h4>
+                                Participe de nossas news com promoções e novidades!
+                            </h4>
+                        </div>
+                        <div id="newsletter_form">
+                            <div className="input_container">
+                                <input className={errorName ? 'input_error' : ''} type="text" onChange={(name_digitted) => {
+                                    setName(name_digitted.target.value)
+                                }} placeholder='Digite seu nome' />
+                                <span className="error_msg">{errorName}</span>
+                            </div>
+                            <div className="input_container">
+                                <input className={errorEmail ? 'input_error' : ''} type="text" onChange={(email_digitted) => {
+                                    setEmail(email_digitted.target.value)
+                                }} placeholder='Digite seu e-mail...' />
+                                <span className="error_msg">{errorEmail}</span>
+                            </div>
+                            <button id="btn_send" onClick={handleSubmit}>Eu quero!</button>
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
     )
 
 }
